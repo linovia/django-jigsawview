947bb051d01a352da4923cdcbc72cb5854b07cc2
Xavier Ordoquy <xordoquy@linovia.com>
2012-06-11 11:28:38 +0200

    Setup creation.

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+Django JigsawView
+=================
+
+:copyright: (c) 2012 by Linovia.
+:license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+
+tests_require = [
+    'mock',
+]
+
+install_requires = [
+    'Django>=1.4',
+]
+
+setup(
+    name='django-jigsawview',
+    version='0.0.1',
+    author='Xavier Ordoquy',
+    author_email='xordoquy@linovia.com',
+    url='https://github.com/linovia/django-jigsawview',
+    description='Create your view like a jigsaw.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+    license='BSD',
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python'
+        'Environment :: Web Environment',
+    ],
+)
