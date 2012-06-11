4a2d0a5211a7ac20e8cf6880566a22463708e9d9
Xavier Ordoquy <xordoquy@linovia.com>
2012-06-11 11:32:41 +0200

    jigsawview package created.

---
+++ jigsawview/__init__.py
@@ -0,0 +1,13 @@
+"""
+jigsawview
+~~~~~~~~~~
+
+:copyright: (c) 2012 by Linovia, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+try:
+    VERSION = __import__('pkg_resources') \
+        .get_distribution('alternative_views').version
+except Exception, e:
+    VERSION = 'unknown'
