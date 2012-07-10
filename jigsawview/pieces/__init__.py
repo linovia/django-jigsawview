24201c09065815b620631fff63edc7c4ddd50b28
Xavier Ordoquy <xordoquy@linovia.com>
2012-07-11 00:39:41 +0200

    Moved the pieces base classes to base.py

--- jigsawview/pieces/__init__.py
+++ jigsawview/pieces/__init__.py
@@ -1,27 +1,5 @@
 """
-Base classes for the jigsawview pieces
+Classes for the jigsawview pieces
 """
 
-
-class BasePiece(object):
-
-    # Tracks each time a Field instance is created. Used to retain order.
-    creation_counter = 0
-
-    def __init__(self, *args, **kwargs):
-        self.type = None
-        super(BasePiece, self).__init__(*args, **kwargs)
-
-        # Increase the creation counter, and save our local copy.
-        self.creation_counter = BasePiece.creation_counter
-        BasePiece.creation_counter += 1
-
-
-class Piece(BasePiece):
-    template_name = None
-
-    def get_template_name(self, *args, **kwargs):
-        return self.template_name
-
-    def get_context_data(self, context, mode, *args, **kwargs):
-        return context
+from jigsawview.pieces.base import BasePiece, Piece
