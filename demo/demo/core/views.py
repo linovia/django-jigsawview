3c586a1a99d752bce203ecee5dbd9ccbcba56eb4
Xavier Ordoquy <xordoquy@linovia.com>
2012-08-29 09:26:33 +0200

    Updated the demo to match the current code implementation.

--- demo/demo/core/views.py
+++ demo/demo/core/views.py
@@ -15,8 +15,8 @@ class ProjectMixin(ObjectPiece):
     def get_success_url(self, obj=None):
         return reverse('projects')
 
-    def get_queryset(self, request, context):
-        return Project.objects.filter(members=request.user.id)
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user.id)
 
 
 class MilestoneMixin(ObjectPiece):
@@ -26,8 +26,8 @@ class MilestoneMixin(ObjectPiece):
     def get_success_url(self):
         return reverse('milestones')
 
-    def get_queryset(self, request, context):
-        return Milestone.objects.filter(project=context['project'])
+    def get_queryset(self):
+        return Milestone.objects.filter(project=self.view.context['project'])
 
 
 class BugMixin(ObjectPiece):
@@ -37,12 +37,12 @@ class BugMixin(ObjectPiece):
     def get_success_url(self, obj=None):
         return reverse('bugs', kwargs={'project_id': obj.project.id})
 
-    def get_queryset(self, request, context):
+    def get_queryset(self):
         # Limits the bugs to the current project's ones
         # and possibly the milestone if we have one
-        qs = Bug.objects.filter(project=context['project'])
-        if 'milestone' in context:
-            qs = qs.filter(milestone=context['milestone'])
+        qs = Bug.objects.filter(project=self.view.context['project'])
+        if 'milestone' in self.view.context:
+            qs = qs.filter(milestone=self.view.context['milestone'])
         return qs
 
 
